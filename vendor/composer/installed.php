72365a31e649ec16944a1e920f150597bc1635d6
Salym Mbisse <105650566+devmpemba@users.noreply.github.com>
2024-06-06 17:38:12 +0300

    updating composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'salimmbise/tanzania-regions',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f4f2f3db7ba5c873c50188b261a62d3a51a77018',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'salimmbise/tanzania-regions' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f4f2f3db7ba5c873c50188b261a62d3a51a77018',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
